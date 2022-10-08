d92a7e1eedd62cdd1e433e0503e93cbb7eca8c90
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-10-08 08:59:23 +0300

    created the basic app  structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div>
+     <h1>
+      Hello world
+     </h1>
+    </div>
+  )
+}
+
+export default App
